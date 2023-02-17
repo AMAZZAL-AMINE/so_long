2163df673f255bea36bb4261ff733c8298b7bfbe
Mohamed Amine Amazzal <mamazzal@c1r1p3.1337.ma>
2023-02-17 12:13:52 +0100

    init update norminet

--- mapers_cheker.c
+++ mapers_cheker.c
@@ -1,156 +1,187 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapers_cheker.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamazzal <mamazzal@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 11:19:22 by mamazzal          #+#    #+#             */
+/*   Updated: 2023/02/17 11:36:10 by mamazzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
-bool check_the_parrent_path(char **map) {
-    return true;
-}
-bool check_path_player(char **map) {
-    int count = 0;
-    int index = 0;
+bool check_path_player(char **map)
+{
+	int	count;
+	int	index;
 
-    while (map[count]) {
-        index = 0;
-        while (map[count][index]) {
-            if (map[count][index] == 'P') {
-                if (map[count - 1][index] == '1' && map[count + 1][index] == '1' && map[count][index + 1] == '1' && map[count][index - 1] == '1') {
-                    printf("\033[31mBruh, player Not found way):\033[0m\n");
-                    exit(1);
-                }
-            }
-            index++;
-        }
-        count++;
-    }
-    return true;
-}
-
-bool check_path_coing(char **map) {
-    int count = 0;
-    int index = 0;
-
-    while (map[count]) {
-        index = 0;
-        while (map[count][index]) {
-            if (map[count][index] == 'C') {
-                if (map[count - 1][index] == '1' && map[count + 1][index] == '1' && map[count][index + 1] == '1' && map[count][index - 1] == '1') {
-                    printf("\033[31mThers No way to COIN ):\033[0m\n");
-                    exit(1);
-                }
-            }
-            index++;
-        }
-        count++;
-    }
-    return true;
+	count = 0;
+	while (map[count])
+	{
+		index = 0;
+		while (map[count][index])
+		{
+			if (map[count][index] == 'P')
+			{
+				if (map[count - 1][index] == '1' && map[count + 1][index] == '1' && map[count][index + 1] == '1' && map[count][index - 1] == '1')
+				{
+					printf("\033[31mBruh, player Not found way):\033[0m\n");
+					exit(1);
+				}
+			}
+			index++;
+		}
+		count++;
+	}
+	return (true);
 }
 
-bool check_path_door(char **map) {
-    int count = 0;
-    int index = 0;
+bool	check_path_coing(char **map)
+{
+	int	count;
+	int	index;
 
-    while (map[count]) {
-        index = 0;
-        while (map[count][index]) {
-            if (map[count][index] == 'E') {
-                if (map[count - 1][index] == '1' && map[count + 1][index] == '1' && map[count][index + 1] == '1' && map[count][index - 1] == '1') {
-                    printf("\033[31mThers No way to DOOR ):\033[0m\n");
-                    exit(1);
-                }
-            }
-            index++;
-        }
-        count++;
-    }
-    return true;
+	count = 0;
+	while (map[count])
+	{
+		index = 0;
+		while (map[count][index])
+		{
+			if (map[count][index] == 'C')
+			{
+				if (map[count - 1][index] == '1'
+					&& map[count + 1][index] == '1'
+					&& map[count][index + 1] == '1'
+					&& map[count][index - 1] == '1')
+					error_int_map();
+			}
+			index++;
+		}
+		count++;
+	}
+	return (true);
 }
 
-bool check_lines_map(char **map) {
-    int count = 0;
-    int index = 0;
+bool	check_path_door(char **map)
+{
+	int	count;
+	int	index;
 
-    while (map[count]) {
-        index = 0;
-        if (map[count + 1] != '\0') {
-            if (ft_strlen(map[count]) != ft_strlen(map[count + 1])) {
-                printf("\033[31mEach Line in map shold be same ):\033[0m\n");
-                exit(1);
-            }
-        }else {
-            if (ft_strlen(map[count]) != ft_strlen(map[count - 1])) {
-                printf("\033[31mEach Line in map shold be same ):\033[0m\n");
-                exit(1);
-            }
-        }
-        count++;
-    }
-    return true;
+	count = 0;
+	while (map[count])
+	{
+		index = 0;
+		while (map[count][index])
+		{
+			if (map[count][index] == 'E')
+			{
+				if (map[count - 1][index] == '1'
+					&& map[count + 1][index] == '1'
+					&& map[count][index + 1] == '1'
+					&& map[count][index - 1] == '1')
+					error_int_map();
+			}
+			index++;
+		}
+		count++;
+	}
+	return (true);
 }
 
-bool check_for_imposter_in_map(char **map) {
-    int count = 0;
-    int index = 0;
+bool	check_lines_map(char **map)
+{
+	int	count;
+	int	index;
 
-    while (map[count]) {
-        while (map[count][index]) {
-            if (map[count][index] != '1' && map[count][index] != '0' && map[count][index] != 'P' && map[count][index] != 'C' && map[count][index] != 'E') {
-                printf("\033[31mBruh, you have imposter in Map At Line : %d::%d ):\033[0m\n", count + 1, index + 1);
-                exit(1);
-            }
-            index++;
-        }
-        index = 0;
-        count++;
-    }
-    return true;
+	count = 0;
+	while (map[count])
+	{
+		index = 0;
+		if (map[count + 1] != '\0')
+		{
+			if (ft_strlen(map[count]) != ft_strlen(map[count + 1]))
+				error_int_map();
+		}
+		else
+		{
+			if (ft_strlen(map[count]) != ft_strlen(map[count - 1]))
+				error_int_map();
+		}
+		count++;
+	}
+	return (true);
 }
 
-//norm => 5 function end here
+bool	check_for_imposter_in_map(char **map)
+{
+	int	count;
+	int	index;
 
-bool check_for_door(char **map) {
-    int count = 0;
-    int index = 0;
-    int check = 0;
-    while (map[count]) {
-        index = 0;
-        while (map[count][index]) {
-            if (map[count][index] == 'E') {
-                check++;
-            }
-            index++;
-        }
-        count++;
-    }
-
-    if (check != 1 ) {
-        if (check == 0)
-            printf("\033[31mBruh,  U do not have door ):\033[0m\n");
-        else 
-            printf("\033[31mBruh, you have Duplacted Door ):\033[0m\n");
-        exit(1);
-    }
-    return true;
+	count = 0;
+	while (map[count])
+	{
+		while (map[count][index])
+		{
+			if (map[count][index] != '1'
+				&& map[count][index] != '0' && map[count][index] != 'P'
+				&& map[count][index] != 'C' && map[count][index] != 'E')
+				error_int_map();
+			index++;
+		}
+		index = 0;
+		count++;
+	}
+	return (true);
 }
 
-bool check_for_player(char **map) {
-    int count = 0;
-    int index = 0;
-    int check = 0;
-    while (map[count]) {
-        index = 0;
-        while (map[count][index]) {
-            if (map[count][index] == 'P') {
-                check++;
-            }
-            index++;
-        }
-        count++;
-    }
+// norm => 5 function end here
 
-    if (check != 1 ) {
-        if (check == 0)
-            printf("\033[31mBruh,  Select Your player ):\033[0m\n");
-        else 
-            printf("\033[31mBruh, you have Multi player ):\033[0m\n");
-        exit(1);
-    }
-    return true;
+bool	check_for_door(char **map)
+{
+	int	count;
+	int	index;
+	int	check;
+
+	check = 0;
+	count = 0;
+	while (map[count])
+	{
+		index = 0;
+		while (map[count][index])
+		{
+			if (map[count][index] == 'E')
+				check++;
+			index++;
+		}
+		count++;
+	}
+	if (check != 1)
+		error_int_map();
+	return (true);
+}
+
+bool	check_for_player(char **map)
+{
+	int	count;
+	int	index;
+	int	check;
+
+	check = 0;
+	count = 0;
+	while (map[count])
+	{
+		index = 0;
+		while (map[count][index])
+		{
+			if (map[count][index] == 'P')
+				check++;
+			index++;
+		}
+		count++;
+	}
+	if (check != 1)
+		error_int_map();
+	return (true);
 }
--- valid_path_map.c
+++ valid_path_map.c
@@ -1,79 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   valid_path_map.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamazzal <mamazzal@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 11:37:16 by mamazzal          #+#    #+#             */
+/*   Updated: 2023/02/17 11:45:00 by mamazzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
-int count_y(char **str)
+int	check_position_player(char c)
 {
-    int count = 0;
-    while (str[count])
-    {
-        count++;
-    }
-    return count;
+	if (c == '0' || c == 'C')
+		return (1);
+	return (0);
 }
 
-int count_x(char **str)
+char	**check_path(char **str)
 {
-    int count = 0;
-    while (str[0][count])
-    {
-        count++;
-    }
-    return count;
-}
+	int	count;
+	int	index;
 
-char *from_2d_to_1d(char **s)
-{
-    char **old_map = s;
-    char *new_map = malloc((count_y(old_map) * count_x(old_map)) * sizeof(char *));
-    int count = 0;
-    int index = 0;
-    int c = 0;
-    while (old_map[count])
-    {
-        index = 0;
-        while (old_map[count][index])
-        {
-            new_map[c] = old_map[count][index];
-            index++;
-            c++;
-        }
-        new_map[c++] = '\n';
-        count++;
-    }
-    new_map[c] = 0;
-    return new_map;
-}
-
-char **check_path(char **str)
-{
-    int count;
-    int index;
-
-    count = 0;
-    while (str[count])
-    {
-        index = 0;
-        while (str[count][index]) {
-            if (str[count][index] == 'P') {
-                if (str[count + 1][index] == 'C' || str[count + 1][index] == '0')  {
-                    str[count + 1][index] = 'P';
-                    count++;
-                }
-                if (str[count - 1][index] == 'C' || str[count - 1][index] == '0')  {
-                    str[count - 1][index] = 'P';
-                    count--;
-                }
-                if (str[count][index + 1] == 'C' || str[count][index + 1] == '0')  {
-                    str[count][index + 1] = 'P';
-                    index++;
-                }
-                 if (str[count][index - 1] == 'C' || str[count][index - 1] == '0')  {
-                    str[count][index - 1] = 'P';
-                    index--;
-                }
-            }
-            index++;
-        }
-        count++;
-    }
-    return str;
+	count = 0;
+	while (str[count])
+	{
+		index = 0;
+		while (str[count][index])
+		{
+			if (str[count][index] == 'P')
+			{
+				if (check_position_player(str[count + 1][index]))
+				{
+					str[count + 1][index] = 'P';
+					count++;
+				}
+				if (check_position_player(str[count - 1][index]))
+				{
+					str[count - 1][index] = 'P';
+					count--;
+				}
+				if (check_position_player(str[count][index + 1]))
+				{
+					str[count][index + 1] = 'P';
+					index++;
+				}
+				if (check_position_player(str[count][index - 1]))
+				{
+					str[count][index - 1] = 'P';
+					index--;
+				}
+			}
+			index++;
+		}
+		count++;
+	}
+	return (str);
 }
